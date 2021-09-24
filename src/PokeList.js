7194e9b44428f17bdd8d61b046fa6a07176c4bf0
Karl Sigel <heykarl@gmail.com>
2021-09-23 18:06:10 -0700

    added search and dropdown functions

--- src/PokeList.js
+++ src/PokeList.js
@@ -5,11 +5,17 @@ export default class PokeList extends Component {
     render() {
         return (
             <ul>
+                {this.props.pokes.map((poke) => {
+                    return (
                 <PokeItem 
-                url_image={}
-                pokemon={}
-                egg_group_1={}
+                key={poke.pokemon}
+                url_image={poke.url_image}
+                pokemon={poke.pokemon}
+                egg_group_1={poke.egg_group_1}
                 />
+                    )
+                }
+                )}
             </ul>
         )
     }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
 import PokeList from './PokeList'
+import request from 'superagent'
 
 export default class SearchPage extends Component {
 
 state = {
-    sortOrder: [],
-    query: []
+    sortOrder: '',
+    query: '',
+    pokes: []
 }
+
+changeSortOrder = (e) => {
+this.setState({ sortOrder: e.target.value })
+}
+
+handleSubmit = async (e) => {
+    e.preventDefault()
+    this.fetchStuff()
+}
+
+searchData = (e) => {
+this.setState({query: e.target.value})
+}
+
+componentDidMount = async () => {
+    this.fetchStuff()
+}
+
+fetchStuff = async () => {
+    const data = await request.get(`https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.query}&sort=pokemon&direction=${this.state.sortOrder}`)
+    this.setState({ pokes: data.body.results })
+}
+
     render() {
         return (
             <div>
-                <form>
-                <input></input>
-                <select>
-                    <option>asc</option>
-                    <option>desc</option>
+                <form onSubmit={this.handleSubmit}>
+                <input onChange={this.searchData} />
+                <select onChange={this.changeSortOrder}>
+                    <option value='asc'>asc</option>
+                    <option value='desc'>desc</option>
                 </select>
                 <button>button</button>
                 </form>
-                <PokeList />
+                <PokeList pokes={this.state.pokes}
+                />
             </div>
         )
     }
