c7cc40a7ef203b9bface5da8c4b2618a3ac34baf
Karl Sigel <heykarl@gmail.com>
2021-09-27 14:12:02 -0700

    set up home page, added detail page doc, added header with nav, added router stuff to app page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
-import SearchPage from './SearchPage.js'
-
+import SearchPage from './SearchPage';
+import DetailPage from './DetailPage';
+import HomePage from './HomePage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <>
-        <SearchPage />
-      </>
+
+<Router>
+  <Switch>
+  <Route 
+      path="/" 
+      exact
+      render={(routerProps) => <HomePage {...routerProps} />} 
+    />
+  <Route 
+      path="/pokemon" 
+      exact
+      render={(routerProps) => <SearchPage {...routerProps} />} 
+    />        
+  <Route 
+      path="/pokemon/:pokeid" 
+      exact
+      render={(routerProps) => <DetailPage {...routerProps} />} 
+    />                  
+  </Switch>
+</Router>
     )
   }
 }
+
+
---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Header from './Header'
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                this is a home page
+            <Header />
+            </div>
+        )
+    }
+}
